6b55b5ae8c5a9582dc317081cdc1e8e2e3d3f94f
Freeman.Xiong <xiongchenyu6@gmail.com>
2017-03-09 12:09:03 +0800

    bootup files

---
+++ dots/installer.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# If symlinks already exists, they are skipped, otherwise destiny files are moved to dotfiles 
+# folder and respective symlinks created.
+# Note: A backup folder is created with all .dotfiles (not symlinks) desired
+############################
+
+########## Variables
+TIME=$(date "+%Y-%m-%d_%H-%M-%S")
+DIR=~/dotfiles # dotfiles directory
+OLDDIR=~/dotfiles_old_"$TIME" # old dotfiles backup directory
+FILES=(ctags muttrc tmux.conf vimrc zshrc) # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
+mkdir -p "$OLDDIR"
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $DIR directory"
+cd $DIR
+echo "...done"
+
+# copy any existing dotfiles (follow symlinks) in homedir to dotfiles_old_$time directory
+for FILE in "${FILES[@]}"; do
+echo "Backup dotfile $FILE from ~ to $OLDDIR"
+cp -L ~/$FILE "$OLDDIR"
+done
+
+# move any existing dotfiles (not symlinks) in homedir to dotfiles directory and create respective symlinks
+for FILE in "${FILES[@]}"; do
+echo "Moving and creating symlink to $FILE from ~ to $DIR."
+ln -sfn $DIR/$FILE ~/.$FILE
+done
+
+source ~/.zshrc
+source ~/.vimrc
--- git/Install.md
+++ git/Install.md
@@ -3,3 +3,14 @@
 echo "tags" >> ~/.global_ignore
 git config --global core.excludesfile $HOME/.global_ignore
 ```
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts/
