42d98dcf11ada3e338fe459314968d89607810fb
xiongchenyu6 <xiongchenyu6@gmail.com>
2017-05-15 18:28:14 +0800

    update

--- dots/xmonad/xmonad.hs
+++ dots/xmonad/xmonad.hs
@@ -2,17 +2,19 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Config
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myManageHook = composeAll
-  [ className =? "emacs"      --> doShift "1:dev"
-  , className =? "emacsclient"  --> doShift "1:dev"
-  , className =? "chromium-browser"  --> doShift "2:web"
-  ,className =? "Chromium"       --> doShift "2:web"
-  , className =? "Google-chrome"  --> doShift "2:web"
-  , manageDocks
+  [ className =? "emacs"      --> doShift "dev"
+  , className =? "emacsclient"  --> doShift "dev"
+  , className =? "chromium-browser"  --> doShift "web"
+  , className =? "Chromium-browser"  --> doShift "web"
+  ,className =? "Chromium"       --> doShift "web"
+  , className =? "Google-chrome"  --> doShift "web"
   ]
 
 main = do
@@ -20,12 +22,14 @@ main = do
 
     xmonad $ defaultConfig
         { workspaces = ["1:dev","2:web","3:game","4:comm","5:ham","6:tmp","7:dvi","8","9","0","-","="]
-        ,manageHook = myManageHook <+> manageHook defaultConfig
+        , manageHook    = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
+        , handleEventHook = ewmhDesktopsEventHook
+        , startupHook   = ewmhDesktopsStartup <+> setWMName "LG3D"
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
