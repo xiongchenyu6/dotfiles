20f70f8699e8ac7ff3c4539d069b0ba5ee3fe137
freeman <xiongchenyu6@gmail.com>
2022-10-17 23:09:08 +0800

    add oci arm host capacity service

--- nixos/server.nix
+++ nixos/server.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, lib, ... }:
-{
-  imports =
-    let
-      ls = dir: builtins.map (f: (dir + "/${f}")) (builtins.attrNames (builtins.readDir dir));
-    in
-    [ ]
-    ++ (ls ./common-apps)
-    ++ (ls ./server-apps)
-    ++ (ls ./common-components)
-    ++ (ls ./server-components)
-  ;
-  services =
-    { };
-
+{ config, pkgs, lib, ... }: {
+  imports = let
+    ls = dir:
+      builtins.map (f: (dir + "/${f}"))
+      (builtins.attrNames (builtins.readDir dir));
+  in [ ] ++ (ls ./common-apps) ++ (ls ./server-apps) ++ (ls ./common-components)
+  ++ (ls ./server-components);
+  services = {
+    oci-arm-host-capacity =
+      let envPath = ../common/oci-arm-host-capacity.secret;
+      in {
+        enable = true;
+        envPath = "${envPath}";
+      };
+  };
 }
