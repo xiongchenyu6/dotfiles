d90e24e6a44320f0a6aaf3909fbba6e161fc2969
freeman <xiongchenyu6@gmail.com>
2022-11-08 18:40:50 +0800

    remove packages

--- profiles/client-pkgs/darwin.nix
+++ profiles/client-pkgs/darwin.nix
@@ -14,6 +14,7 @@
       jetbrains.idea-community
       dbeaver
       vscode
+      pkgs.emacsGitNativeComp
     ];
   };
   homebrew = {
@@ -31,6 +32,67 @@
       autoUpdate = true;
       cleanup = "uninstall";
       upgrade = true;
+    };
+  };
+
+  services = {
+    netbird = { enable = true; };
+    postgresql = {
+      enable = true;
+      enableTCPIP = true;
+    };
+    redis = { enable = true; };
+    skhd = {
+      enable = true;
+      skhdConfig = "alt + shift - r : chunkc quit";
+    };
+    spacebar = {
+      enable = true;
+      config = {
+        clock_format = "%R";
+        background_color = "0xff202020";
+        foreground_color = "0xffa8a8a8";
+      };
+    };
+    yabai = {
+      enable = true;
+      config = {
+        focus_follows_mouse = "autoraise";
+        mouse_follows_focus = "off";
+        window_placement = "second_child";
+        window_opacity = "off";
+        top_padding = 36;
+        bottom_padding = 10;
+        left_padding = 10;
+        right_padding = 10;
+        window_gap = 10;
+      };
+    };
+    system = {
+      defaults = {
+        NSGlobalDomain = {
+          InitialKeyRepeat = 180;
+          KeyRepeat = 60;
+          AppleTemperatureUnit = "Celsius";
+          AppleShowAllFiles = true;
+        };
+        dock = {
+          autohide = true;
+          dashboard-in-overlay = true;
+          mru-spaces = false;
+        };
+        finder = {
+          AppleShowAllExtensions = true;
+          AppleShowAllFiles = true;
+          QuitMenuItem = true;
+          ShowPathbar = true;
+          ShowStatusBar = true;
+        };
+      };
+      keyboard = {
+        enableKeyMapping = true;
+        remapCapsLockToControl = true;
+      };
     };
   };
 }
