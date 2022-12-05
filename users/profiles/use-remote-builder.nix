bf8930235d4e51e09d20286bf1f9aad0895963c2
freeman <xiongchenyu6@gmail.com>
2022-12-05 13:18:36 +0800

    Feature: move root ssh users to user specified

--- users/profiles/use-remote-builder.nix
+++ users/profiles/use-remote-builder.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
+{lib, ...}: {
   home = {
     stateVersion = "22.11";
   };
@@ -12,7 +12,7 @@
       compression = true;
       matchBlocks = {
         "*.trontech.link" = {
-          user = "freeman.xiong";
+          user = lib.mkDefault "freeman.xiong";
         };
       };
       extraConfig = ''
