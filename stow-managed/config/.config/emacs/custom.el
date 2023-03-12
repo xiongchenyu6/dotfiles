17f05122c54cf3217ccbb96de5d68363f53546ba
freeman <xiongchenyu6@gmail.com>
2023-03-12 18:54:49 +0800

    add game token

--- stow-managed/config/.config/emacs/custom.el
+++ stow-managed/config/.config/emacs/custom.el
@@ -9,8 +9,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dap-auto-configure-mode nil)
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(org-re-reveal ace-link all-the-icons blamer cider citre cmake-mode cape corfu editorconfig citeproc dockerfile-mode direnv doom-modeline emacsql-sqlite-builtin format-all gradle-mode graphviz-dot-mode gnu-apl-mode gcmh go-mode kind-icon leetcode lispy ligature lsp-haskell lsp-java nix-mode meow magit-todos marginalia magit-gitflow nov ox-hugo ob-mermaid ob-restclient ox-pandoc org-contrib org-roam org-download orderless plantuml-mode pdf-tools protobuf-mode rime rainbow-delimiters racket-mode reformatter rg rust-mode sly solidity-flycheck scala-mode sbt-mode tide tempel vterm wakatime-mode which-key yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
