d2367deac7013a4cfd242121bb7c7c46905b6a4e
freeman <xiongchenyu6@gmail.com>
2023-08-09 09:05:23 +0800

    add openvpn template

--- hosts/nixos/openVPN/default.nix
+++ hosts/nixos/openVPN/default.nix
@@ -5,6 +5,23 @@ let
   domain = "3.82.157.47";
   vpn-dev = "tun0";
   port = 1194;
+
+  openvpn = {
+    ca = ./vpn/ca.crt;
+    cert = ./vpn/server.crt;
+    key = ./vpn/server.key;
+    ta = ./vpn/ta.key;
+    dh = ./vpn/dh2048.pem;
+
+    client_subnet = "10.89.98.0";
+    client_mask_bits = 24;
+    client_mask = "255.255.255.0";
+
+    forward_to_subnet = "192.168.4.0";
+    forward_to_mask_bits = 24;
+    forward_to_mask = "255.255.255.0";
+  };
+
 in {
 
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
@@ -22,24 +39,50 @@ in {
 
   environment.systemPackages = [ pkgs.openvpn ]; # for key generation
 
-  services.openvpn.servers.smartphone.config = ''
-    dev ${vpn-dev}
-    proto udp
-    ifconfig 10.8.0.1 10.8.0.2
-    secret ${client-key}
-    port ${toString port}
+  services.openvpn.servers = {
+    # peer2peer.config = ''
+    #   dev ${vpn-dev}
+    #   proto udp
+    #   ifconfig 10.8.0.1 10.8.0.2
+    #   secret ${client-key}
+    #   port ${toString port}
 
-    cipher AES-256-CBC
-    auth-nocache
+    #   cipher AES-256-CBC
+    #   auth-nocache
 
-    comp-lzo
-    keepalive 10 60
-    ping-timer-rem
-    persist-tun
-    persist-key
-  '';
+    #   comp-lzo
+    #   keepalive 10 60
+    #   ping-timer-rem
+    #   persist-tun
+    #   persist-key
+    # '';
+    server.config = ''
+      port 1194
+      proto udp
+      dev tun
+      ca ${openvpn.ca}
+      cert ${openvpn.cert}
+      key ${openvpn.key}
+      dh ${openvpn.dh}
+      tls-auth ${openvpn.ta} 0
 
-  environment.etc."openvpn/smartphone-client.ovpn" = {
+      server ${openvpn.client_subnet} ${openvpn.client_mask}
+      keepalive 10 120
+      comp-lzo
+      max-clients 5
+      user nobody
+      group nogroup
+      persist-key
+      persist-tun
+      verb 6
+      reneg-sec 0
+
+      push "route ${openvpn.forward_to_subnet} ${openvpn.forward_to_mask}"
+      push "redirect-gateway def1" # https://openvpn.net/index.php/open-source/documentation/howto.html#redirect
+    '';
+  };
+
+  environment.etc."openvpn/peer2peer.ovpn" = {
     text = ''
       dev tun
       remote "${domain}"
@@ -62,7 +105,7 @@ in {
     mode = "600";
   };
   system.activationScripts.openvpn-addkey = ''
-    f="/etc/openvpn/smartphone-client.ovpn"
+    f="/etc/openvpn/peer2peer.ovpn"
     if ! grep -q '<secret>' $f; then
       echo "appending secret key"
       echo "<secret>" >> $f
