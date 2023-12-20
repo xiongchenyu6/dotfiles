c7ed03b0c43d3505a74fe57796d6a17608426d51
xiongchenyu <xiongchenyu6@gmail.com>
2023-12-20 22:48:27 +0800

    update packages

--- users/profiles/gui/window-manager/hyprland/nvidia.nix
+++ users/profiles/gui/window-manager/hyprland/nvidia.nix
@@ -12,7 +12,7 @@
   wayland = {
     windowManager = {
       hyprland = {
-        enableNvidiaPatches = true;
+        #        enableNvidiaPatches = true;
 
         extraConfig = lib.mkDefault ''
           general {
