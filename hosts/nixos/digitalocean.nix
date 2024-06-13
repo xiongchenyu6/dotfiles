19b5ef946287dfe7361aeca361988af492b9d691
xiongchenyu <xiongchenyu6@gmail.com>
2024-06-13 18:26:22 +0800

    fix digital ocean

--- hosts/nixos/digitalocean.nix
+++ hosts/nixos/digitalocean.nix
@@ -1,8 +1,10 @@
-{ modulesPath, ... }:
+{ lib, modulesPath, ... }:
 {
   imports = [
     ./base.nix
     "${modulesPath}/virtualisation/digital-ocean-image.nix"
   ];
+  boot.loader.grub.devices = lib.mkForce [ "/dev/vda" ];
+  boot.loader.grub.device = "/dev/vda";
   virtualisation.digitalOceanImage.compressionMethod = "bzip2";
 }
