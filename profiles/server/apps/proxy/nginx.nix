fdd87d9c7961622ad782081ecafd481c10457e0a
xiongchenyu <xiongchenyu6@gmail.com>
2024-07-20 15:21:56 +0800

    test gitlens

--- profiles/server/apps/proxy/nginx.nix
+++ profiles/server/apps/proxy/nginx.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, ... }:
 {
-
   services = {
     nginx = {
       enable = true;
@@ -8,31 +7,20 @@
       recommendedProxySettings = true;
       #      gitweb = { enable = true; };
       additionalModules = [ pkgs.nginxModules.pam ];
+      eventsConfig = ''
+        worker_connections 20000;
+      '';
+      appendHttpConfig = ''
+        include ${./log};
+        error_log syslog:server=unix:/dev/log;
+        access_log syslog:server=unix:/dev/log main;
+      '';
     };
-    prometheus.exporters = {
-      nginx = {
-        enable = true;
-        port = 9005;
-      };
-    };
-  };
-
-  systemd.services.nginx.serviceConfig = {
-    SupplementaryGroups = [ "shadow" ];
-    NoNewPrivileges = lib.mkForce false;
-    PrivateDevices = lib.mkForce false;
-    ProtectHostname = lib.mkForce false;
-    ProtectKernelTunables = lib.mkForce false;
-    ProtectKernelModules = lib.mkForce false;
-    RestrictAddressFamilies = lib.mkForce [ ];
-    LockPersonality = lib.mkForce false;
-    MemoryDenyWriteExecute = lib.mkForce false;
-    RestrictRealtime = lib.mkForce false;
-    RestrictSUIDSGID = lib.mkForce false;
-    SystemCallArchitectures = lib.mkForce "";
-    ProtectClock = lib.mkForce false;
-    ProtectKernelLogs = lib.mkForce false;
-    RestrictNamespaces = lib.mkForce false;
-    SystemCallFilter = lib.mkForce "";
+    # prometheus.exporters = {
+    #   nginx = {
+    #     enable = true;
+    #     port = 9005;
+    #   };
+    # };
   };
 }
