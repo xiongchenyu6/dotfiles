5c325d74f54023b642a3431c01ff8980caa29083
xiongchenyu <xiongchenyu6@gmail.com>
2024-07-26 20:35:46 +0800

    update deps

--- profiles/common/components/virtualisation.nix
+++ profiles/common/components/virtualisation.nix
@@ -4,28 +4,27 @@
       enable = true;
       qemu.package = pkgs.qemu_kvm;
     };
-    docker = {
-      enable = true;
-      autoPrune = {
-        enable = true;
-        flags = [ "--all" "--force" ];
-      };
-    };
-    # podman = {
+    # docker = {
     #   enable = true;
     #   autoPrune = {
     #     enable = true;
     #     flags = [ "--all" "--force" ];
     #   };
-    #   dockerSocket.enable = true;
-    #   defaultNetwork.settings.dns_enabled = true;
-    #   dockerCompat = true;
-    #   # networkSocket = {
-    #   #   enable = true;
-    #   #   server = "ghostunnel";
-    #   # };
-
     # };
+    podman = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        flags = [ "--all" "--force" ];
+      };
+      dockerSocket.enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+      dockerCompat = true;
+      # networkSocket = {
+      #   enable = true;
+      #   server = "ghostunnel";
+      # };
+     };
   };
   environment.systemPackages = with pkgs; [ virt-manager ];
 
