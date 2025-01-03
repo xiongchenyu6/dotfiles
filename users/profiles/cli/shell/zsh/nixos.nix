83c46d857d701bee3e5c0f2a141093aa9ec5fc65
xiongchenyu <freemanxiong00@gmail.com>
2025-01-03 21:06:12 +0800

    update

--- users/profiles/cli/shell/zsh/nixos.nix
+++ users/profiles/cli/shell/zsh/nixos.nix
@@ -9,10 +9,11 @@
       initExtra = ''
         eval $(${pkgs.bash-my-aws}/bin/bma-init)
         eval "$(${pkgs.github-copilot-cli}/bin/github-copilot-cli alias -- "$0")"
-        complete -C '${pkgs.awscli2}/bin/aws_completer' aws
+              complete -C '${pkgs.awscli2}/bin/aws_completer' aws
         eval "$(${pkgs.rustup}/bin/rustup completions zsh)"
         eval "$(${pkgs.grafana-loki}/bin/logcli --completion-script-zsh)"
       '';
+
     };
   };
 }
