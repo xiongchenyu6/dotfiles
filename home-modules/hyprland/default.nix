9b9c466f9e2b2a6c6ce95c973287a53b1eac2c39
xiongchenyu <xiongchenyu6@gmail.com>
2025-02-24 17:20:33 +0800

    update code

--- home-modules/hyprland/default.nix
+++ home-modules/hyprland/default.nix
@@ -24,233 +24,124 @@
       INPUT_METHOD = "fcitx";
       XIM_SERVERS = "fcitx";
       NIXOS_OZONE_WL = "1";
-      # WLR_NO_HARDWARE_CURSORS = "1";
       XDG_SESSION_TYPE = "wayland";
-      # LSP_USE_PLISTS = "true";
-      #NIX_SSHOPTS = "-Y";
       XDG_SESSION_DESKTOP = "Hyprland";
     };
   };
-  wayland = {
-    windowManager = {
-      hyprland =
-        let
-          clean-up-after-start = pkgs.writeShellApplication {
-            name = "clean-up-after-start.sh";
-            runtimeInputs = [ pkgs.hyprland ];
-            text = ''
-              sleep 10
-              hyprctl keyword windowrule "workspace unset,microsoft-edge"
-            '';
-          };
 
-          workspace = pkgs.writeShellApplication {
-            name = "workspace.sh";
-            runtimeInputs = [
-              pkgs.hyprland
-              pkgs.gnugrep
-              pkgs.gawk
-            ];
-            text = ''
-              monitors=/tmp/hypr/monitors_temp
-              hyprctl monitors > $monitors
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
 
-              if [[ -z $1 ]]; then
-                workspace=$(grep -B 4 "focused: no" "$monitors" | awk 'NR==1 {print $3}')
-              else
-                workspace=$1
-              fi
+    settings = {
+      monitor = [
+        ", highrr, auto, auto"
+        "HDMI-A-1, preferred, auto, 1, transform, 1"
+      ];
 
-              activemonitor=$(grep -B 7 "focused: yes" "$monitors" | awk 'NR==1 {print $2}')
-              passivemonitor=$(grep  -B 3 "($workspace)" "$monitors" | awk 'NR==1 {print $2}')
-              #activews=$(grep -A 2 "$activemonitor" "$monitors" | awk 'NR==3 {print $1}' RS='(' FS=')')
-              passivews=$(grep -A 2 "$passivemonitor" "$monitors" | awk 'NR==4 {print $1}' RS='(' FS=')')
+      workspace = [
+        "HDMI-A-1, 1"
+      ];
 
-              if [[ $workspace -eq $passivews ]] && [[ $activemonitor != "$passivemonitor" ]]; then
-                hyprctl dispatch swapactiveworkspaces "$activemonitor" "$passivemonitor"
-                echo "$activemonitor" "$passivemonitor"
-              else
-                hyprctl dispatch moveworkspacetomonitor "$workspace $activemonitor" && hyprctl dispatch workspace "$workspace"
-              fi
-            '';
-          };
-        in
-        {
-          enable = true;
-          xwayland = {
-            enable = true;
-            # hidpi = true;
-          };
+      xwayland = {
+        force_zero_scaling = true;
+      };
 
-          extraConfig = lib.mkDefault ''
-            monitor=,highrr,auto,auto
+      input = {
+        kb_layout = "us";
+        kb_options = "ctrl:nocaps";
+        repeat_rate = 60;
+        repeat_delay = 180;
+        follow_mouse = 1;
+        sensitivity = 1.0;
 
-            monitor=HDMI-A-1,preferred,auto,1,transform,1
-
-            workspace=HDMI-A-1,1
-            # unscale XWayland
-            xwayland {
-              force_zero_scaling = true
-            }
-            input {
-            kb_layout = us
-
-            kb_options = ctrl:nocaps
-
-            kb_rules =
-            repeat_rate = 60
-            repeat_delay = 180
-            # left_handed = 1
-            follow_mouse = 1
-            # unscale XWayland
-            xwayland {
-              
-            }
-            touchpad {
-            natural_scroll = no
-            }
-            sensitivity = 1.0 # -1.0 - 1.0, 0 means no modification.
-            }
-
-            general {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            # no_cursor_warps = 1
-            gaps_in = 5
-            gaps_out = 15
-            border_size = 2
-            col.active_border = rgba(1affffee)
-            col.inactive_border = rgba(595959aa)
-
-            layout = dwindle
-            # env = QT_QPA_PLATFORMTHEME,qt6ct
-            # toolkit-specific scale
-            env = GDK_SCALE,1.5
-            env = XCURSOR_SIZE,32
-            }
-
-            decoration {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            rounding = 10                        
-            }
-
-            animations {
-            enabled = yes
-
-            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
-            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-            animation = windows, 1, 7, myBezier
-            animation = windowsOut, 1, 7, default, popin 80%
-            animation = border, 1, 10, default
-            animation = fade, 1, 7, default
-            animation = workspaces, 1, 6, default
-            }
-
-            dwindle {
-            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-            pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-            preserve_split = yes # you probably want this
-            }
-
-            gestures {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            workspace_swipe = on
-            }
-            # Example windowrule v1
-            # windowrule = float, ^(kitty)$
-            # Example windowrule v2
-            # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-            # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-
-            # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-            $mainMod = SUPER
-
-            # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-            bind = $mainMod, return, exec, alacritty
-            bind = $mainMod, X, exec, albert toggle
-
-            # workspaces
-            #binds mod + [shift +] {1..10} to [move to] ws {1..10}
-            bind = $mainMod, c, killactive,
-            bind = $mainMod SHIFT, Q, exit,
-            bind = $mainMod, L, togglefloating,
-            bind = $mainMod, V, pseudo, # dwindle
-            bind = $mainMod, W, swapactiveworkspaces, eDP-1 HDMI-A-1
-            bind = $mainMod, T, togglesplit, # dwindle
-            bind = $mainMod, G, togglegroup, # dwindle
-            bind = $mainMod, M ,layoutmsg, swapwithmaster
-            bind = $mainMod SHIFT, M, layoutmsg, focusmaster
-            bind = $mainMod, space, fullscreen, # dwindle
-
-            # Move focus with mainMod + arrow keys
-            bind = $mainMod, B, movefocus, l
-            bind = $mainMod, F, movefocus, r
-            bind = $mainMod, P, movefocus, u
-            bind = $mainMod, N, movefocus, d
-            bind = $mainMod, 25, focusmonitor, l
-            bind = $mainMod, 26, focusmonitor, r
-
-            # Scroll through existing workspaces with mainMod + scroll
-            bind = $mainMod SHIFT, mouse_down, workspace, e+1
-            bind = $mainMod SHIFT, mouse_up, workspace, e-1
-
-            ${builtins.concatStringsSep "\n" (
-              builtins.genList (
-                x:
-                let
-                  ws =
-                    let
-                      c = (x + 1) / 10;
-                    in
-                    builtins.toString (x + 1 - (c * 10));
-                in
-                ''
-                  # bind = $mainMod, ${ws}, workspace, ${toString (x + 1)}
-                  bind=$mainMod,${ws},exec, ${workspace}/bin/workspace.sh ${toString (x + 1)}
-                  bind = $mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}
-                ''
-              ) 10
-            )}
-            # cycle monitors
-
-            # Move/resize wndows with mainMod + LMBi/RMB and dragging
-            bindm = $mainMod SHIFT, mouse:272, movewindow
-            bindm = $mainMod, mouse:273, resizewindow
-
-            # volumn
-            binde=, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+
-            binde=, XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-            bind=, XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-            bind=, XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-
-            # brightness
-            binde=, XF86MonBrightnessUp, exec, brightnessctl s +5%
-            binde=, XF86MonBrightnessDown, exec, brightnessctl s 5%-
-
-            # window resize
-            bind = $mainMod, S, submap, resize
-            submap = resize
-            binde = , right, resizeactive, 10 0
-            binde = , left, resizeactive, -10 0
-            binde = , up, resizeactive, 0 -10
-            binde = , down, resizeactive, 0 10
-            bind = , escape, submap, reset
-            submap = reset
-
-            exec-once = albert
-
-            windowrule=workspace 1 silent,alacritty
-            windowrule=float,noblur,noshadow,noborder,pseudo,dimaround,albert
-
-            exec-once=netbird-ui
-
-            # exec-once=${clean-up-after-start}/bin/clean-up-after-start.sh
-
-            windowrule = opacity 0.9 0.95,Alacritty
-            windowrule = opacity 0.9 0.95,emacs
-          '';
+        touchpad = {
+          natural_scroll = false;
         };
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 15;
+        border_size = 2;
+        "col.active_border" = "rgba(1affffee)";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+
+        env = [
+          "GDK_SCALE,1.5"
+          "XCURSOR_SIZE,32"
+        ];
+      };
+
+      animations = {
+        enabled = true;
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      gestures = {
+        workspace_swipe = true;
+      };
+
+      "$mod" = "SUPER";
+
+      bind = [
+        "$mod, return, exec, alacritty"
+        "$mod, X, exec, albert toggle"
+        "$mod, c, killactive"
+        "$mod SHIFT, Q, exit"
+        "$mod, L, togglefloating"
+        "$mod, V, pseudo"
+        "$mod, W, swapactiveworkspaces, eDP-1 HDMI-A-1"
+        "$mod, T, togglesplit"
+        "$mod, G, togglegroup"
+        "$mod, M, layoutmsg, swapwithmaster"
+        "$mod SHIFT, M, layoutmsg, focusmaster"
+        "$mod, space, fullscreen"
+        "$mod, B, movefocus, l"
+        "$mod, F, movefocus, r"
+        "$mod, P, movefocus, u"
+        "$mod, N, movefocus, d"
+        "$mod, 25, focusmonitor, l"
+        "$mod, 26, focusmonitor, r"
+      ];
+
+      binde = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+      ];
+
+      exec-once = [
+        "albert"
+        "netbird-ui"
+      ];
+
+      windowrule = [
+        "workspace 1 silent, alacritty"
+        "float,noblur,noshadow,noborder,pseudo,dimaround,albert"
+        "opacity 0.9 0.95,Alacritty"
+        "opacity 0.9 0.95,emacs"
+      ];
     };
   };
 
