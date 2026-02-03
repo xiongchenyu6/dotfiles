8b117b515bc527f0cbc19352e749d34309303301
xiongchenyu <xiongchenyu6@gmail.com>
2026-02-03 11:05:43 +0800

    Update oracle-arm-001: Add auth.autolife.ai nginx vhost with CORS support

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -21,6 +21,37 @@ let
 in
 {
   imports = [
+    ({ config, inputs, lib, ... }:
+      {
+        disabledModules = [
+          "${inputs.srvos}/nixos/common"
+          "${inputs.srvos}/nixos/common/default.nix"
+        ];
+
+        imports = [
+          (inputs.srvos + "/shared/common/flake.nix")
+          (inputs.srvos + "/nixos/common/detect-hostname-change.nix")
+          (inputs.srvos + "/nixos/common/networking.nix")
+          (inputs.srvos + "/nixos/common/nix.nix")
+          (inputs.srvos + "/nixos/common/openssh.nix")
+          (inputs.srvos + "/nixos/common/serial.nix")
+          (inputs.srvos + "/nixos/common/sudo.nix")
+          (inputs.srvos + "/nixos/common/update-diff.nix")
+          (inputs.srvos + "/shared/common/well-known-hosts.nix")
+          (inputs.srvos + "/nixos/common/zfs.nix")
+        ];
+
+        services.userborn.enable = lib.mkIf (
+          !(
+            (config.environment ? persistence && config.environment.persistence != { })
+            || (lib.any (u: u.subUidRanges != [ ] || u.autoSubUidGidRange) (lib.attrValues config.users.users))
+          )
+        ) (lib.mkDefault true);
+
+        boot.initrd.systemd.enable = lib.mkDefault (!config.boot.swraid.enable && !config.boot.isContainer);
+        environment.ldso32 = null;
+        boot.tmp.cleanOnBoot = lib.mkDefault true;
+      })
     ezModules.kernel
     ezModules.security
     ezModules.ssh-harden
